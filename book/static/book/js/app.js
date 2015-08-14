a4c933e7c8e9c33de6a799736f6a04141321aed2
Sumit Jamgade <sumitj@invicto.in>
2015-08-14 12:46:36 +0530

    edit button in snippet working partially
    It shows the only sippet being edited

--- book/static/book/js/app.js
+++ book/static/book/js/app.js
@@ -11,13 +11,24 @@ angular.module('player',[])
         angular.forEach(response,function(song){
             $scope.list[song.id]=song;
             $scope.list[song.id].loaded=0;
+            angular.forEach($scope.list[song.id].snippets, function(snip){
+                snip.editing = false;
+            });
         });
         $scope.snippets = $scope.list[1].snippets;
+        $scope.editing = false
     }).error(function(error){
         $scope.state = 'errored in gettting data';
         console.log('Errored!!!!\n'+error);
     });
     $scope.loadSong = function(song_id){
         $scope.snippets = $scope.list[song_id].snippets;
+        $scope.editing = false;
+    }
+    $scope.editSnip = function(snip_id){
+        angular.forEach($scope.snippets, function(snip){
+            snip.editing = snip.id == snip_id ? ! snip.editing : snip.editing;
+        });
+        $scope.editing = ! $scope.editing;
     }
 })
