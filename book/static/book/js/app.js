7be6cd01f4fb4726d0f96b6e044352e983d8cf71
Sumit Jamgade <sumitj@invicto.in>
2015-08-24 12:59:23 +0530

    player is loading the given song. Modifying snippet locally.

--- book/static/book/js/app.js
+++ book/static/book/js/app.js
@@ -5,7 +5,7 @@ angular.module('player',[])
 })
 .controller('songlist',function($scope, $http){
     $scope.state = 'gettingdata'
-    $http.get('/book/songs').success(function(response){
+    $http.get('/book/songs/').success(function(response){
         $scope.list = {};
         $scope.snippets = {} 
         $scope.state = ["Getting", "Getting"]
@@ -17,22 +17,32 @@ angular.module('player',[])
             });
         });
         $scope.snippets = $scope.list[1].snippets;
-        $scope.editing = false
+        $scope.editing = false;
+        $scope.player = videojs('playerbox',{"preload":"auto", "controls":true, "autoplay":true});
     }).error(function(error){
         console.log('Errored!!!!\n'+error);
     });
     $scope.loadSong = function(song_id){
         $scope.snippets = $scope.list[song_id].snippets;
         $scope.editing = false;
+        $scope.state = [" Edit", "Delete"];
+        song=$scope.list[song_id];
+        console.log($scope.player.src({src:"/static/"+song.path, type:"video/"+song.type}));
+        $scope.player.play();
     };
     $scope.editSnip = function(snip_id){
         angular.forEach($scope.snippets, function(snip){
             snip.editing = snip.id == snip_id ? ! snip.editing : snip.editing;
         });
         $scope.editing = ! $scope.editing;
-        $scope.state = $scope.editing ? ["Undo","Save"] : [" Edit", "Cancel"];
+        $scope.state = $scope.editing ? ["Undo","Save"] : [" Edit", "Delete"];
     };
     $scope.saveSnip = function(snip_id){
-        $scope.editSnip(snip_id)
+        $scope.snippets[snip_id].start = $scope.currentStart;
+        $scope.snippets[snip_id].end = $scope.currentEnd;
+        $scope.editSnip(snip_id);
     };
+    $scope.getCurrent = function(){
+        return 99;
+    }
 })
